a6c29ecb40cdcda1cf38abd6570f29eb31ea5fca
Tomas Herrou <54865094+therrou@users.noreply.github.com>
2024-09-14 21:54:42 +0200

    test payload

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -5,6 +5,9 @@ export async function GET() {
   return NextResponse.json({ message: "Hello World from GET" });
 }
 
-export async function POST() {
-  return NextResponse.json({ message: "Hello World from POST" });
+export async function POST(request: Request, response: Response) {
+  console.log("HERE ====================== REQUEST", request);
+  return new Response('Success from NEXT JS', {
+    status: 200,
+  })
 }
